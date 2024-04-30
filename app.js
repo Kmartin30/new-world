0c2a6d76d9faa176affea0fc0eab0eebcb93d7d2
Kmartin30 <mkdedra@outlook.com>
2024-04-30 13:59:18 -0500

    Programmed JS

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 'use strict'; 
+
 console.log ("In the HTML");
 alert ("Welcome to Finances");
 
+function getEmail() {
+    const userEmail = prompt("What is your email"); 
+    //declaring a constant variable called userEmail, assigning it the value of what the prompt returns (the user's input)
+return userEmail;
+} 
+const theirEmail = getEmail(); 
+console.log(theirEmail);  
 
-const userEmail = prompt("What is your email");
-console.log(userEmail);
 
-document.write(" Welcome " + userEmail + " to Financial Education" );
-document.write("<h3>Mission & Values</h3><p>Have a nice day!</p>")
+function greetUser(){
+    document.write(" Welcome " + theirEmail + " to Financial Education" );
+}
+
+greetUser(); 
+
+
 
 if(userEmail == "@gmail.com") {alert("Hey Google User");
 }
 
 let person = prompt("Please Enter Your Name", "Code Fellows");
 let knowledge = confirm ("Do you know financial literacy?"); 
+
+function printContact(){
+    alert("Name + Phone Number");
+
+}
+
+
+
+//---Functions are ways to create commands---//
+
+//---pararmeters - placeholders names for inforamation the function will need to do its job---//
